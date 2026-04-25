abb9b2d3aa076cb6f6bd88db2f2c5ce36a5de1b0
sebastiansanchezpi21-pixel <sebastiansanchezpi21@gmail.com>
2026-04-25 12:46:48 -0500

    Add step definition for verifying product count

---
+++ src/test/java/edu/pe/cibertec/saucedemo/stepdefinitions/ProductosStepDefinitions.java
@@ -0,0 +1,17 @@
+package com.saucedemo.stepdefinitions;
+
+import com.saucedemo.screenplay.tasks.VerificarInventario;
+import io.cucumber.java.en.Then;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ProductosStepDefinitions {
+
+    @Then("she should see exactly {int} products on the inventory page")
+    public void sheShouldSeeExactlyProductsOnTheInventoryPage(int cantidad) {
+        OnStage.theActorInTheSpotlight().should(
+                seeThat(VerificarInventario.cantidadDeProductos(), equalTo(cantidad))
+        );
+    }
+}
