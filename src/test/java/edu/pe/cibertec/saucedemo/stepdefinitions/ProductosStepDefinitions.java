19ad5dfd2457ddd56da5b5bbe8206e9c6debbc31
sebastiansanchezpi21-pixel <sebastiansanchezpi21@gmail.com>
2026-04-25 14:18:30 -0500

    Refactor product count verification for readability

--- src/test/java/edu/pe/cibertec/saucedemo/stepdefinitions/ProductosStepDefinitions.java
+++ src/test/java/edu/pe/cibertec/saucedemo/stepdefinitions/ProductosStepDefinitions.java
@@ -2,6 +2,7 @@ package com.saucedemo.stepdefinitions;
 
 import com.saucedemo.screenplay.tasks.VerificarInventario;
 import io.cucumber.java.en.Then;
+import net.serenitybdd.screenplay.actors.OnStage;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -11,7 +12,10 @@ public class ProductosStepDefinitions {
     @Then("she should see exactly {int} products on the inventory page")
     public void sheShouldSeeExactlyProductsOnTheInventoryPage(int cantidad) {
         OnStage.theActorInTheSpotlight().should(
-                seeThat(VerificarInventario.cantidadDeProductos(), equalTo(cantidad))
+                seeThat(
+                        VerificarInventario.cantidadDeProductos(),
+                        equalTo(cantidad)
+                )
         );
     }
 }
